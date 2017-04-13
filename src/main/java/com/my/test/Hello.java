ce88cabdedd38f3700a4759d99625f65dad9641c
sai.luo <sai.luo@qunar.com>
2017-04-13 16:43:51 +0800

    测试

---
+++ src/main/java/com/my/test/Hello.java
@@ -0,0 +1,13 @@
+package com.my.test;
+
+/**
+ * Created by luosai on 17-4-13.
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("----------------");
+        System.out.println("Hello World!");
+        System.out.println("----------------");
+    }
+}
