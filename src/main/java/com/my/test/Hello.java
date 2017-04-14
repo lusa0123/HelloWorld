490801bf03ff23632aa95383e93e41108fe958e1
sai.luo <sai.luo@qunar.com>
2017-04-14 10:13:11 +0800

    test

--- src/main/java/com/my/test/Hello.java
+++ src/main/java/com/my/test/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("----------------");
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!");
         System.out.println("----------------");
     }
 }
